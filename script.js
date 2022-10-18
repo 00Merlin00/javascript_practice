ef544c36dda496f46cdc4130f54424e81b299627
00Merlin00 <mrautela12@gmail.com>
2022-10-18 19:26:21 +0530

    some commit

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-// console.log("hello world");
-let firstName = "harshit"
-firstName = firstName.slice(1,5);
-console.log(firstName);
+// // console.log("hello world");
+// let firstName = "harshit"
+// firstName = firstName.slice(1,5);
+// console.log(firstName);
+
+let num = 15;
+
+if (num%2===0) {
+    console.log ("even")
+}else {
+    console.log ("odd")
+}
