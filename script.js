71550b3766b568e9f1905e2720ccbb476e6c1275
Mohit Rautela <mrautela12@gmail.com>
2022-10-23 22:56:13 +0530

    some commit

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-const myArray = ["value1", "value2"];
+const person = { name:"Harshit",age:"22",hobbies:["sleeping","cooking","music"]};
 
-// let myvar1 =  myArray[0];
-// let myvar2 =  myArray[1];
+person.gender = "male";
 
-// console.log( "value of myvar1" , myvar1);
-// console.log( "value of myvar2" , myvar2);
-
-let [myvar1 , myvar2] = myArray;
-
-console.log ( myArray);
-
-console.log( "value of myvar1" , myvar1);
-console.log( "value of myvar2" , myvar2);
+console.log( person);
