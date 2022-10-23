bade5bc8f4dec7faffebb962838f913e0f8b37fc
Mohit Rautela <mrautela12@gmail.com>
2022-10-23 21:19:52 +0530

    some commit

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-const fruits = ["mango","apple","banana","grapes"]
+const myArray = ["value1", "value2"];
 
-let i = 0;
+// let myvar1 =  myArray[0];
+// let myvar2 =  myArray[1];
 
-while ( i<fruits.length){
+// console.log( "value of myvar1" , myvar1);
+// console.log( "value of myvar2" , myvar2);
 
-console.log(
-    fruits[i]
-); i++;
-}
+let [myvar1 , myvar2] = myArray;
+
+console.log ( myArray);
+
+console.log( "value of myvar1" , myvar1);
+console.log( "value of myvar2" , myvar2);
