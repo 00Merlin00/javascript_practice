98b0332195a987ddc10295c317526180ca88d5ed
Mohit Rautela <mrautela12@gmail.com>
2022-10-25 21:56:33 +0530

    some commit

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-// nested destructuring
+function solutionMaking ( x,y) {
+    
 
-const users = [
-    {userId: 1, firstName: "harshit", gender:'male'},
-    {userId: 2, firstName: "mohit" , gender: "male"},
-    {userId: 3, firstName: "nitish" , gender: "male"},
-]
+   return 
+}
 
-const [ user1, user2, user3] = users;
-console.log(user1);
+const solving = solutionMaking(3,5);
+
+
+
+console.log (solving);
