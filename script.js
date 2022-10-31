f30b63ef85a2d09ad3c604e7115cbd147b25b20d
Mohit Rautela <mrautela12@gmail.com>
2022-10-31 06:11:09 +0530

    some commit

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const numbers = [ 1,3,2,6,4,8];
+const userCart = [
+{productId : 1 , productName : "mobile" , price : 12000 },
+{productId : 2 , productName : "laptop" , price : 22000 },
+{productId : 3 , productName : "tv" , price : 15000 }
+]
 
-function isEven (number){
-    return number % 2 === 0 ;
-}
+const totalPrice = userCart.reduce(function(accumulator,currentvalue){
+return accumulator + currentvalue.price
+},0)
 
-const newArray = numbers.filter(isEven);
-
-console.log( newArray);
+console.log(totalPrice);
