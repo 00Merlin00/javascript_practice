9af8c3f6c0f624dce3fa3568a8e39f6fe253b6f4
Mohit Rautela <mrautela12@gmail.com>
2022-11-01 12:28:49 +0530

    some commit

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-const products = [
-    { poductId : 1, productName: "p1", price: 300},
-    { poductId : 2, productName: "p2", price: 3000},
-    { poductId : 3, productName: "p3", price: 200},
-    { poductId : 4, productName: "p4", price: 8000},
-    { poductId : 5, productName: "p5", price: 500}
-]
+const person = new Map();
+person.set("firstName", "mohit")
+person.set("age", 22)
 
-products.sort((a,b)=>{
-    return a.price - b.price
-});
 
-console.log( products);
+// console.log(person);
+// console.log(person.get("mohit"))
+
+for(let value of person.values()){
+    console.log(value);
+}
