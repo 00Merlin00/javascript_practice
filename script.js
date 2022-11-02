e56d8ff422d8d9db8619fe5c45be6168d0e0ed9b
Mohit Rautela <mrautela12@gmail.com>
2022-11-03 01:09:16 +0530

    some commit

--- script.js
+++ script.js
@@ -22,3 +22,26 @@ console.log(animal1);
 
 console.log(animal1.isCute());
 console.log(animal1.isSuperCute());
+
+
+
+
+class Dog extends Animals {
+    constructor( name , age , speed){
+        super (name , age) ;
+        this.speed = speed ;
+    }
+
+    run(){
+       return  `${this.name} runs at ${this.speed}kmph`
+    }
+    
+}
+
+const tommy = new Dog( "tommy", 3, 45);
+
+console.log(tommy);
+
+
+console.log(tommy.run());
+
