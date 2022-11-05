3e32744342df340c67f5bdde0a25b3bd06153fe9
Mohit Rautela <mrautela12@gmail.com>
2022-11-06 04:48:04 +0530

    some commit

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-console.log(firstName);
+function hello (x) {
+    const a = "varA" ;
+    const b = "varB" ;
+    return function(){
+        console.log(a,b,x);
+    }
+}
 
-const firstName = "Mohit";
+const ans = hello(arg);
+
+ans() ;
