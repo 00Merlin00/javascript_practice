da7299104897549f92a65aba22296754eec7835f
Mohit Rautela <mrautela12@gmail.com>
2022-11-11 15:26:46 +0530

    some commit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ link.setAttribute("href","https://google.co.in");
 
 // const inputElement = document.querySelector(".form-todo input");
 
-// console.log(inputElement.getAttribute("type")); 
+console.log(inputElement.getAttribute("type")); 
 
 const navItems = document.getElementsByClassName("nav-item");
 console.log(navItems);
