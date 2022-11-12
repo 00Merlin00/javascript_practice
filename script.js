626c09b448ab9427819c3739e68481bc4efd9e99
Mohit Rautela <mrautela12@gmail.com>
2022-11-12 11:43:20 +0530

    some commit

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 // const link = document.querySelector("a");
 // console.log(link.getAttribute("href"));
-link.setAttribute("href","https://google.co.in");
-
+// link.setAttribute("href","https://google.co.in");
 
 // const inputElement = document.querySelector(".form-todo input");
 
-console.log(inputElement.getAttribute("type")); 
+// console.log(inputElement.getAttribute("type"));
 
-const navItems = document.getElementsByClassName("nav-item");
-console.log(navItems);
+// const navItems = document.getElementsByTagName("a");
+// console.log(navItems);
+
+// for(i=0; i<navItems.length; i++){
+//   const navItem = navItems[i];
+//   navItem.style.backgroundColor = "#fff"
+//   navItem.style.color = "green" ;
+//   navItem.style.fontWeight = "bold" ;
+
+// }
+
+// for(let navItem of navItems){
+//     navItem.style.background = "#fff"
+//     navItem.style.color = "white" ;
+//     navItem.style.fontWeight = "bold" ;
+
+// }
+
+// const show = document.getElementsByClassName("nav-item");
+// console.log(show[0].children[1].innerHTML)
+
+
