59c96eb5816456d9108fc896f280e53deb66fe2a
Mohit Rautela <mrautela12@gmail.com>
2022-11-15 22:54:29 +0530

    some commit

--- script2.js
+++ script2.js
@@ -1,9 +1,3 @@
 const allButtons = document.querySelectorAll("button");
 
-function myFunc(button){
-    return button.style.background="yellow" ,
-     button.style.color = "grey" ;
-
-}
-
-allButtons.addEventListener("click" , myFunc)
+button 
