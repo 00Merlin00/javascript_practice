8d299c8659e5a05cf6cf44607bb4c9b7cd1692c2
Mohit Rautela <mrautela12@gmail.com>
2022-11-16 16:08:46 +0530

    some commit

--- script2.js
+++ script2.js
@@ -1,3 +1,8 @@
-const allButtons = document.querySelectorAll("button");
+const allButtons = document.querySelectorAll(".button button");
 
-button 
+for(let button of allButtons) {
+    button.addEventListener("click" , function(e){
+    return e.target.style.color = "blue",
+    e.target.style.background = "yellow"
+    })
+}
