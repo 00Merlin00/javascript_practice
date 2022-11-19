aee635b143bcef1a0037b2c4f965d5afb6925d79
Mohit Rautela <mrautela12@gmail.com>
2022-11-20 01:53:42 +0530

    some commit

--- script.js
+++ script.js
@@ -6,3 +6,5 @@ console.log("script end");
 function addNo (num1,num2){
     console.log(num1+num2)
 }
+
+addNo(1,4);
---
+++ script2.js
@@ -0,0 +1,6 @@
+const heading1 = document.querySelector(".heading1");
+
+setTimeout(()=>{
+heading1.textContent="heading 1"
+heading1.style.color="violet"
+},1000)
